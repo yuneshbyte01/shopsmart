fb9bffcf7ef4d6ba0a67a7abf70a4dd991fae759
Yunesh Timsina <yuneshtimsina@gmail.com>
2025-07-04 15:09:44 +0545

    Initial commit - Spring Boot project setup

---
+++ src/test/java/com/yuneshtimsina/shopsmart/ShopSmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuneshtimsina.shopsmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopSmartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
